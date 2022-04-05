d766311606eae7ccb5a2ec93be5404dc7fc5d99f
Nihed MBAREK <nihedmm@gmail.com>
2022-04-05 11:04:22 +0400

    Create list.rb
    adding a way to list all tables/families

---
+++ list.rb
@@ -0,0 +1,25 @@
+require 'java'
+require 'net/http'
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client.Connection
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.client.Table
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.client.HBaseAdmin
+o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+
+config =  HBaseConfiguration.create()
+connection = ConnectionFactory.createConnection(config);
+admin = connection.getAdmin();
+print "------------------\n"
+print admin.listTables().length();
+print "------------------\n"
+listTables = admin.listTables()
+
+for index in 0 ... listTables.size
+  families = listTables[index].getColumnFamilies()
+  families.each { |x| puts "#{listTables[index].getTableName()} , "+ x.getNameAsString() }
+end
